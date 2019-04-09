ba38372c2dd396f59482dfe96859ee4cdc18175c
Alex Dyksa <odyksa@myagent.com.ua>
2019-04-09 12:47:43 +0300

    added Home, About, Books, Contacts pages

---
+++ src/pages/Books.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Books = () => (
+    <main className="books">
+        <h1>Books</h1>
+    </main>
+);
+
+export default Books;
---
+++ src/pages/Contacts.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Contacts = () => (
+    <main className="contacts">
+        <h1>Contacts</h1>
+    </main>
+);
+
+export default Contacts;
