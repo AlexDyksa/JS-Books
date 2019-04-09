2b197d33c98d50845791c294175c6739f48f3adc
Alex Dyksa <odyksa@myagent.com.ua>
2019-04-09 17:11:44 +0300

    modified Book component

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const Books = () => (
-    <main className="books">
-        <h1>Books</h1>
-    </main>
-);
+import BookCard from '../components/BookCard';
+
+const Books = ({ match, books }) => {
+    const sortedBooks = match.params.topic ? books.filter(book => book.topic === match.params.topic) : books;
+
+    const booksCard = sortedBooks.map((book, index) => <BookCard key={index} book={book} />);
+
+    return (
+        <main className="books">
+            <h1>Books</h1>
+            <section className="books-list">
+                {booksCard}
+            </section>
+        </main>
+    );
+};
 
 export default Books;
