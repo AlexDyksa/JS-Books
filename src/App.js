482844f6c2940e4f0aa60438dfaa3361b716a4d0
Alex Dyksa <odyksa@myagent.com.ua>
2019-04-09 14:41:52 +0300

    added NotFound component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Logo from './components/Logo';
 import Navigation from './components/Navigation';
+import Sidenav from './components/Sidenav';
 
 import Home from './pages/Home';
 import About from './pages/About';
 import Books from './pages/Books';
 import Contacts from './pages/Contacts';
+import NotFound from './pages/NotFound';
 
 class App extends Component {
     render() {
+        const topics = this.props.topics;
+
         return (
             <BrowserRouter>
                 <div className="app">
@@ -20,10 +24,18 @@ class App extends Component {
                         <Navigation />
                     </div>
 
-                    <Route path="/" exact component={Home} />
-                    <Route path="/about" exact component={About} />
-                    <Route path="/books" exact component={Books} />
-                    <Route path="/contacts" exact component={Contacts} />
+                    <div className="app-content">
+                        <Route exact path="/books" render={props => <Sidenav topics={topics} {...props} />}
+                        />
+
+                        <Switch>
+                            <Route path="/" exact component={Home} />
+                            <Route path="/about" component={About} />
+                            <Route path="/books" component={Books} />
+                            <Route path="/contacts" component={Contacts} />
+                            <Route component={NotFound} />
+                        </Switch>
+                    </div>
                 </div>
             </BrowserRouter>
         );
---
+++ src/pages/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => (
+    <main className="notfound">
+        <h1>Sorry, page not found :(</h1>
+    </main>
+);
+
+export default NotFound;
